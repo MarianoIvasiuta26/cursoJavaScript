7e9a852f2c11f546690fd33ba8f8f8e4ffa13124
MarianoIvasiuta26 <81333168+MarianoIvasiuta26@users.noreply.github.com>
2022-08-25 09:40:09 -0300

    ejemplos de operaciones y la funcion predefinida number()

--- scripts.js
+++ scripts.js
@@ -9,8 +9,21 @@ var puntuacion = 0;
 var record;
 record= 5000;
 
-alert("Mi nombre es: " + nombre);
+// Ejemplo --> alert("Mi nombre es: " + nombre);
 
 //Operaciones
-alert((5+5)*55);
 
+// Ejemplo --> alert((5+5)*55);
+
+//EJEMPLO DE OPERACIONES\\
+
+    var precioUnidad, cantidad, total;
+    precioUnidad = 5;
+    cantidad = 100;
+    total = precioUnidad * cantidad;
+
+//FUNCIÓN NUMBER();
+var camisetas, pantalones, totalArticulos;
+camisetas = "5";
+pantalones = 5;
+totalArticulos = Number(camisetas) + pantalones; //Se convirtió el valor de camisetas a un tipo Number
